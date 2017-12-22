d420a31d988cc973589247ed8c0faee2ad22e08f
zolenas <zol.hen.as@gmail.com>
2017-12-22 04:01:31 +0100

    Menu component initialisation

---
+++ src/app/components/task-menu/task-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'task-menu',
+  templateUrl: './task-menu.component.html',
+  styleUrls: ['./task-menu.component.css']
+})
+export class TaskMenuComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
