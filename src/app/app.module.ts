0ba94520c0ebb16061456e048b47143b34ba98f4
zolenas <zol.hen.as@gmail.com>
2017-12-22 04:07:16 +0100

    First design, ergonomy try one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule, MatButtonModule, MatCardModule, MatIconRegistry, MatTooltipModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-
+import { TaskService } from './services/task.service';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { TaskDetailComponent } from './components/task-detail/task-detail.component';
+import { TaskMenuComponent } from './components/task-menu/task-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskItemComponent,
+    TaskDetailComponent,
+    TaskMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTooltipModule
   ],
-  providers: [],
+  providers: [MatIconRegistry, TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
